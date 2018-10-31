08e38f14b227837ad065f44698d7cd7337d1baf8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-31 15:25:19 +0000

    Deploy otushomework/homework04 to github.com/otushomework/homework04.git:gh-pages

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/travis/build/otushomework/homework04
+# Build directory: /home/travis/build/otushomework/homework04
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version_valid "test_version")
