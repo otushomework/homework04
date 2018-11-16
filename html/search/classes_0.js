6379e022d5500e6b82529f0c643db28b3c7cdabf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-16 10:06:04 +0000

    Deploy otushomework/homework04 to github.com/otushomework/homework04.git:gh-pages

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['is_5fcontainer',['is_container',['../structis__container.html',1,'']]],
   ['is_5fcontainer_3c_20std_3a_3alist_3c_20ts_2e_2e_2e_3e_20_3e',['is_container&lt; std::list&lt; Ts...&gt; &gt;',['../structis__container_3_01std_1_1list_3_01Ts_8_8_8_4_01_4.html',1,'']]],
-  ['is_5fcontainer_3c_20std_3a_3avector_3c_20ts_2e_2e_2e_3e_20_3e',['is_container&lt; std::vector&lt; Ts...&gt; &gt;',['../structis__container_3_01std_1_1vector_3_01Ts_8_8_8_4_01_4.html',1,'']]]
+  ['is_5fcontainer_3c_20std_3a_3avector_3c_20ts_2e_2e_2e_3e_20_3e',['is_container&lt; std::vector&lt; Ts...&gt; &gt;',['../structis__container_3_01std_1_1vector_3_01Ts_8_8_8_4_01_4.html',1,'']]],
+  ['is_5fstring',['is_string',['../structis__string.html',1,'']]],
+  ['is_5fstring_3c_20std_3a_3astring_20_3e',['is_string&lt; std::string &gt;',['../structis__string_3_01std_1_1string_01_4.html',1,'']]]
 ];
--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['print_5fip',['print_ip',['../main_8cpp.html#ae2bd6744affef12d9ad0491696cf7aaf',1,'print_ip(const T &amp;ip):&#160;main.cpp'],['../main_8cpp.html#a2331ab88e664fd51ad938932183bbad0',1,'print_ip(T ip):&#160;main.cpp'],['../main_8cpp.html#a6449d274f9c0be5ac3120ae01dc3a788',1,'print_ip(const T &amp;ip):&#160;main.cpp']]]
+  ['print_5fip',['print_ip',['../main_8cpp.html#ae2bd6744affef12d9ad0491696cf7aaf',1,'print_ip(const T &amp;ip):&#160;main.cpp'],['../main_8cpp.html#a66266eea87b4a76f1b7f0e3d1bce33a7',1,'print_ip(T ip):&#160;main.cpp'],['../main_8cpp.html#a6449d274f9c0be5ac3120ae01dc3a788',1,'print_ip(const T &amp;ip):&#160;main.cpp']]]
 ];
